679b56093e096351b591f2af96a2b2b3ba1766c3
Mikhail <shchurousky@gmail.com>
2021-05-17 12:43:03 +0300

    add requirements seq settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k_1el#uh^7i5yyv0anx!%pv*n)&hf2jk!ssm_^!ortxvon20il'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,7 +148,7 @@ INTERNAL_IPS = ['127.0.0.1']
 # Настройка для отправки почты
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mikhail.shchurovsky@gmail.com'
-EMAIL_HOST_PASSWORD = '6374744shchurmi'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
